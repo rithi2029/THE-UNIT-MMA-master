4c89d4e440800252a9031cf635b6876eced76691
iamrithu <ritimahesh29@gmail.com>
2022-08-10 12:59:08 +0530

    Added Product page

--- lib/screens/booking_screen/booking.dart
+++ lib/screens/booking_screen/booking.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:unitmma/constant_widgets/app_bar.dart';
+import 'package:unitmma/scaffold/scaffold.dart';
+import 'package:unitmma/screens/home_screen/home.dart';
+import 'package:unitmma/screens/product_screen/widgets/card.dart';
+import 'package:unitmma/screens/product_screen/widgets/grid_product_list.dart';
+
+import '../../constants/global_variables.dart';
 
 class BookingScreen extends StatelessWidget {
   const BookingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("BookingScreen")),
+    final scaffoldWidth = MediaQuery.of(context).size.width;
+    final scaffoldHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: appBar,
+      body: SizedBox(
+        width: scaffoldWidth,
+        height: scaffoldHeight,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                width: scaffoldWidth,
+                height: scaffoldHeight * 0.3,
+                child: const Cards(
+                  url:
+                      "https://theunitmma.co.uk/wp-content/uploads/2022/02/IMG_9633-scaled.jpg",
+                ),
+              ),
+              SizedBox(
+                width: scaffoldWidth,
+                height: scaffoldHeight * 10,
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: MyWidget(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/product_screen/widgets/grid_product_list.dart
+++ lib/screens/product_screen/widgets/grid_product_list.dart
@@ -1,31 +1,85 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:woocommerce_api/woocommerce_api.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: ((context, constraints) {
-        return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                childAspectRatio: 2 / 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20),
-            itemCount: 10,
-            itemBuilder: (BuildContext ctx, index) {
-              return Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(15)),
-                child: Text("rithi"),
-              );
-            });
-      }),
-    );
+    Future getProducts() async {
+      // Initialize the API
+      WooCommerceAPI wooCommerceAPI = WooCommerceAPI(
+          url: "https://theunitmma.co.uk/",
+          consumerKey: "ck_dc42350e0d839a416bb73fdef9984544907a8fdb",
+          consumerSecret: "cs_023b27d43090a807fc43d77cda696a15cc87442e");
+
+      // Get data using the "products" endpoint
+      final products = await wooCommerceAPI.getAsync("products");
+      return (products);
+    }
+
+    return FutureBuilder<dynamic>(
+        future: getProducts(),
+        builder: (_, snapshot) {
+          if (snapshot.hasData) {
+//hence here snapshot has some data and you can use .lenght on it
+            if (snapshot.data.length > 1) {}
+          } else {
+            print("not working");
+          }
+
+          return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 2 / 3,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20),
+              itemCount: 10,
+              itemBuilder: (BuildContext ctx, index) {
+                return Container(
+                  alignment: Alignment.center,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: LayoutBuilder(builder: ((context, constraints) {
+                      return Container(
+                        width: constraints.maxWidth,
+                        height: constraints.maxHeight,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: constraints.maxWidth,
+                              height: constraints.maxHeight * 0.7,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(0),
+                                child: Image(
+                                  image: NetworkImage(
+                                      snapshot.data[index]["images"][0]["src"]),
+                                ),
+                              ),
+                            ),
+                            Text(snapshot.data[index]["name"].toString()),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Text(snapshot.data[index]["price"]),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    })),
+                  ),
+                );
+              });
+        });
   }
 }
